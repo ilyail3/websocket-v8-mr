b3131a01abaac81b929a13e77f034bf3b529e7b4
Ilya Kramer <ikrammer@datapipe.com>
2016-05-24 17:04:07 -0400

    Looks like the bridge is working as expected now

--- main.cpp
+++ main.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 #include <libwebsockets.h>
 #include <cstring>
+#include <map>
 #include "jsmn.h"
+#include "czmq.h"
 
 // Test sign2
 
 struct per_session_data__dumb_increment {
-    int number;
+    int session_number;
 };
 
+struct connection_details{
+    struct libwebsocket_context *context;
+    struct libwebsocket *sock;
+};
+
+using namespace std;
+
+static map<int, struct connection_details> sessions;
 static unsigned char complete_buffer[2000];
 static unsigned char* result_buffer = complete_buffer + LWS_SEND_BUFFER_PRE_PADDING;
 
 static jsmn_parser parser;
 static jsmntok_t tokens[256];
+
+static zsock_t* sender;
 
 
 int callback_dumb_increment(
@@ -39,15 +51,32 @@ int callback_dumb_increment(
             printf("Client established\n");
             {
                 struct per_session_data__dumb_increment* data = (struct per_session_data__dumb_increment*)user;
-                data->number = 0;
+                data->session_number = rand();
+
+                sessions[data->session_number] = {context, sock};
             }
             break;
         case LWS_CALLBACK_CLOSED:
             printf("Client closed\n");
+
+            {
+                struct per_session_data__dumb_increment* data = (struct per_session_data__dumb_increment*)user;
+
+                sessions.erase(data->session_number);
+            }
             break;
         case LWS_CALLBACK_RECEIVE:
         {
             struct per_session_data__dumb_increment* data = (struct per_session_data__dumb_increment*)user;
+
+            char* zmq_msg = (char*)malloc(len + 4);
+            *(int*)zmq_msg = data->session_number;
+
+            memcpy(zmq_msg + 4, in, len);
+
+            zframe_t* frame = zframe_new(zmq_msg, len+4);
+            zframe_send(&frame, sender, 0);
+            /*struct per_session_data__dumb_increment* data = (struct per_session_data__dumb_increment*)user;
             const char* input = (const char*)in;
 
 
@@ -75,7 +104,8 @@ int callback_dumb_increment(
             printf("data:%s\n", result_buffer);
 
             libwebsocket_write(sock, result_buffer, out_len, LWS_WRITE_TEXT);
-            libwebsocket_callback_on_writable(context, sock);
+            libwebsocket_callback_on_writable(context, sock);*/
+
         }
             break;
         default:
@@ -94,7 +124,7 @@ static struct libwebsocket_protocols protocols[] = {
         NULL
 };
 
-using namespace std;
+
 
 int main() {
 
@@ -116,7 +146,10 @@ int main() {
 
     libwebsocket_context* context = libwebsocket_create_context(&info);
 
+    zsock_t* rv;
 
+    sender = zsock_new_push("@ipc:///tmp/ws.in");
+    rv = zsock_new_pull("@ipc:///tmp/ws.out");
 
     if (context == NULL) {
         lwsl_err("libwebsocket init failed\n");
@@ -126,7 +159,31 @@ int main() {
     bool force_exit = false;
     int n = 0;
 
+    zsock_t* which;
+
+    zpoller_t* poller = zpoller_new(rv, NULL);
+
     while (n >= 0 && !force_exit) {
+        while((which = (zsock_t*)zpoller_wait(poller, 10)) != NULL){
+            zframe_t* frame = zframe_recv(rv);
+            void* data = zframe_data(frame);
+            size_t size = zframe_size(frame);
+
+            int session = *((int*)data);
+
+            printf("recive zmq message for:%d\n", session);
+
+            auto it = sessions.find(session);
+            if(it != sessions.end()){
+                memcpy(result_buffer, data + 4, size - 4);
+
+                libwebsocket_write(it->second.sock, result_buffer, size - 4, LWS_WRITE_TEXT);
+                libwebsocket_callback_on_writable(it->second.context, it->second.sock);
+            }
+
+        }
+
+
         n = libwebsocket_service(context, 50);
     }
 
